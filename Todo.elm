d954a3b0be36111e5d01e5ed9c4bc6a14fa4128d
Joel McCracken <mccracken.joel@gmail.com>
2016-04-25 18:12:46 -0400

    factor task update stuff into own update fn

--- Todo.elm
+++ Todo.elm
@@ -89,49 +89,52 @@ update : Action -> Model -> Model
 update action model =
     case action of
       NoOp -> model
+      TaskAction tu -> task_update tu model
 
-      TaskAction tu ->
-        case tu of
-          Add ->
-            { model |
-                uid = model.uid + 1,
-                newTask = "",
-                tasks =
-                if String.isEmpty model.newTask
-                then model.tasks
-                else model.tasks ++ [newTask model.newTask model.uid]
-            }
-          UpdateNewTask str ->
-              { model | newTask = str }
 
-          EditingTask id isEditing ->
-              let updateTask t = if t.id == id then { t | editing = isEditing } else t
+task_update : Task_Update -> Model -> Model
+task_update tu model =
+    case tu of
+        Add ->
+          { model |
+              uid = model.uid + 1,
+              newTask = "",
+              tasks =
+              if String.isEmpty model.newTask
+              then model.tasks
+              else model.tasks ++ [newTask model.newTask model.uid]
+          }
+        UpdateNewTask str ->
+            { model | newTask = str }
+
+        EditingTask id isEditing ->
+            let updateTask t = if t.id == id then { t | editing = isEditing } else t
               in
-                  { model | tasks = List.map updateTask model.tasks }
+                { model | tasks = List.map updateTask model.tasks }
 
-          UpdateTask id task ->
-              let updateTask t = if t.id == id then { t | description = task } else t
-              in
-                  { model | tasks = List.map updateTask model.tasks }
+        UpdateTask id task ->
+            let updateTask t = if t.id == id then { t | description = task } else t
+            in
+                { model | tasks = List.map updateTask model.tasks }
 
-          Delete id ->
-              { model | tasks = List.filter (\t -> t.id /= id) model.tasks }
+        Delete id ->
+            { model | tasks = List.filter (\t -> t.id /= id) model.tasks }
 
-          DeleteComplete ->
-              { model | tasks = List.filter (not << .completed) model.tasks }
+        DeleteComplete ->
+            { model | tasks = List.filter (not << .completed) model.tasks }
 
-          Check id isCompleted ->
-              let updateTask t = if t.id == id then { t | completed = isCompleted } else t
-              in
-                  { model | tasks = List.map updateTask model.tasks }
+        Check id isCompleted ->
+            let updateTask t = if t.id == id then { t | completed = isCompleted } else t
+            in
+                { model | tasks = List.map updateTask model.tasks }
 
-          CheckAll isCompleted ->
-              let updateTask t = { t | completed = isCompleted }
-              in
-                  { model | tasks = List.map updateTask model.tasks }
+        CheckAll isCompleted ->
+            let updateTask t = { t | completed = isCompleted }
+            in
+                { model | tasks = List.map updateTask model.tasks }
 
-          ChangeVisibility visibility ->
-              { model | visibility = visibility }
+        ChangeVisibility visibility ->
+            { model | visibility = visibility }
 
 ---- VIEW ----
 
